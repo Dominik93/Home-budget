2feea2dbcf46c875b898827e9f859705335355cc
Dominik93 <slusarz.dominik@gmail.com>
2015-03-20 19:35:17 +0100

    database connection, token work

--- app/src/main/java/com/paradowski/slusarz/homebudget/Token.java
+++ app/src/main/java/com/paradowski/slusarz/homebudget/Token.java
@@ -1,11 +1,20 @@
 package com.paradowski.slusarz.homebudget;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -13,7 +22,7 @@ import java.util.Random;
  */
 public class Token {
 
-    private String token = null;
+    private String token = "";
     private final String FILENAME = "hb_config";
     private Context context;
 
@@ -26,19 +35,16 @@ public class Token {
     }
 
     public void createToken() throws IOException {
-        if(!loadToken()){
-            this.token = Long.toHexString(Double.doubleToLongBits(Math.random()));
-            saveToken();
-        }
+        this.token = Long.toHexString(Double.doubleToLongBits(Math.random())) + Long.toHexString(Double.doubleToLongBits(Math.random()));
     }
 
-    private void saveToken() throws IOException {
+    public void saveToken() throws IOException {
         FileOutputStream fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
         fos.write(this.token.getBytes());
         fos.close();
     }
 
-    private boolean loadToken() throws IOException {
+    public boolean loadToken() throws IOException {
         try {
             FileInputStream fin = context.openFileInput(FILENAME);
             int c;
@@ -50,8 +56,11 @@ public class Token {
             fin.close();
             return true;
         }catch(FileNotFoundException e){
+            Log.e("FileNotFoundException", e.getMessage());
             return false;
         }
     }
 
 }
+
+
